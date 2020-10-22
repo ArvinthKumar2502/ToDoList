39b666adbfbbf8adfda38db20149a13106178305
Arvinth Kumar <arvinthkumar.s@gmail.com>
2020-10-22 19:59:03 +0530

    Add project files.

---
+++ ToDoList/Models/ToDo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ToDoList.Models
+{
+    public class ToDo
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public DateTime DueDate { get; set; }
+        public string Attributes { get; set; }
+        public string Priority { get; set; }
+
+        public virtual ApplicationUser User { get; set; }
+        public bool isDone { get; set; }
+    }
+}
